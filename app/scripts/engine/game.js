baddeef0dc4213b033743f152a61c01cb5459b63
Blaise <blaise.gaillard@gmail.com>
2014-09-06 13:19:23 -0400

    Nome robot functionality

---
+++ app/scripts/engine/game.js
@@ -0,0 +1,33 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+var robot = new Robot(100,100);
+var then = Date.now();
+
+// Robot image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+    bgReady = true;
+};
+bgImage.src = "resources/background.jpg";
+
+// The main game loop
+var main = function () {
+    var now = Date.now();
+    var delta = now - then;
+
+    robot.update(delta/1000);
+    ctx.drawImage(bgImage, 0, 0);
+    robot.render(ctx);
+    console.log('robot! ', robot);
+    then = now;
+
+    // Request to do this again ASAP
+    requestAnimationFrame(main);
+};
+main();
