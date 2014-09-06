88ce3f101141dc4e4b89d46854420404cf2cd825
Jeremy Dunsworth <jdunsworth@learningobjects.com>
2014-09-06 14:45:46 -0400

    poorly size canvas and add background tile

--- app/scripts/directives/gameCanvas.js
+++ app/scripts/directives/gameCanvas.js
@@ -5,21 +5,25 @@ angular.module('gamejamApp')
             replace: true,
             templateUrl: 'scripts/directives/gameCanvas.html',
             link: function(scope, element) {
+                var windowHeight = window.innerHeight;
+
                 var canvas = $(element).get(0);
                 var ctx = canvas.getContext("2d");
-                canvas.width = 512;
-                canvas.height = 480;
+                canvas.height = windowHeight;
+                canvas.width = windowHeight * 4 / 3;
+
 
                 var robot = new Robot(100, 100);
                 var then = Date.now();
 
                 // Robot image
-                var bgReady = false;
                 var bgImage = new Image();
                 bgImage.onload = function() {
-                    bgReady = true;
+                    var ptrn = ctx.createPattern(bgImage, 'repeat'); // Create a pattern with this image, and set it to "repeat".
+                    ctx.fillStyle = ptrn;
+                    ctx.fillRect(0, 0, canvas.width, canvas.height); // context.fillRect(x, y, width, height);
                 };
-                bgImage.src = "images/background.png";
+                bgImage.src = "images/backgroundTile.png";
 
                 // The main game loop
                 var mainGame = function() {
@@ -27,6 +31,7 @@ angular.module('gamejamApp')
                     var delta = now - then;
 
                     robot.update(delta / 1000);
+
                     ctx.drawImage(bgImage, 0, 0);
                     robot.render(ctx);
                     //console.log('robot! ', robot);
