32532622ecba13bc517d6e97745741af79443dba
Blaise <blaise.gaillard@gmail.com>
2014-09-06 16:25:03 -0400

    Simplified turning

--- app/scripts/engine/Robot.js
+++ app/scripts/engine/Robot.js
@@ -11,7 +11,7 @@ var Robot = function(x,y) {
 	var self = this;
     this.x = x;
     this.y = y;
-    this.angle = 0;
+    this.direction = 'right';
     this.image = rbImage;
     this.busy = false;
 
@@ -21,9 +21,7 @@ var Robot = function(x,y) {
     this.speed = 20;
 
     this.turning = false;
-    this.turnedDistance = 0;
-    // radians/second
-    this.turnSpeed = 5;
+    this.turningDirection = 'right';
 
     this.instructions = {
     	"moveForward": function(params) {
@@ -33,9 +31,9 @@ var Robot = function(x,y) {
             self.busy = true;
     	},
     	"turn": function(params) {
-    		// params[0] is the new angle
+    		// params[0] is the direction
             self.turning = true;
-            self.turnedDistance = params[0];
+            self.turningDirection = params[0];
     	}
     };
 };
@@ -50,7 +48,6 @@ Robot.prototype.render = function(canvasSize, squareSize, ctx) {
         this.x*squareSize.width - squareSize.width/2,
         this.y*squareSize.height - squareSize.height/2
     );
-    ctx.rotate(this.angle);
 
     ctx.drawImage(this.image, -squareSize.width/2, -squareSize.width/2, squareSize.width, squareSize.height);
 
@@ -67,7 +64,7 @@ Robot.prototype.update = function(time) {
         this.$moveForward(time);
     }
     if (this.turning) {
-        this.$turn(time);
+        this.$turn();
     }
 }
 
@@ -88,16 +85,39 @@ Robot.prototype.$moveForward = function(time) {
     }
 }
 
-Robot.prototype.$turn = function(time) {
-    if (this.turnedDistance > 0) {
-        var modifier = this.turnSpeed * time;
-        this.turnedDistance -= modifier;
-        this.angle += modifier;
+Robot.prototype.$turn = function() {
+    if (this.turningDirection == 'right') {
+        switch(this.direction)     {
+            case 'up':
+                this.direction = 'right';
+                break;
+            case 'right':
+                this.direction = 'down';
+                break;
+            case 'down':
+                this.direction = 'left';
+                break;
+            case 'left':
+                this.direction = 'up';
+                break;
+        }
     } else {
-        this.turning = false;
-        this.turnedDistance = 0;
-        this.busy = false;
+        switch(this.direction)     {
+            case 'up':
+                this.direction = 'left';
+                break;
+            case 'right':
+                this.direction = 'up';
+                break;
+            case 'down':
+                this.direction = 'right';
+                break;
+            case 'left':
+                this.direction = 'down';
+                break;
+        }
     }
+    this.turning = false;
 }
 
 // RobotIO
